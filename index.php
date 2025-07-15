4116561d93a0c76ba9d721177767ae8a5f6e9f0d
presicesb <elvisargenisjesusdiazochoa11@gmail.com>
2025-07-15 03:28:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php include('includes/header.php'); ?>
-
-<!-- No hay contenido principal en index, es una redirección -->
-
-<?php include('includes/footer.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Continue</title>
+<meta content="0;URL=login.php" http-equiv="REFRESH"/>
+</head>
+<body>
